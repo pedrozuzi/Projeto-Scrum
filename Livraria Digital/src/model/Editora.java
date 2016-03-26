795af87617e5f2bc5f0960e2efd57f760ed47ad3
Zuzi <pedropz0750@gmail.com>
2016-03-26 11:45:29 -0300

    editora model

--- Livraria Digital/src/model/Editora.java
+++ Livraria Digital/src/model/Editora.java
@@ -1,5 +1,35 @@
 package model;
 
 public class Editora {
+	
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String cnpj;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
 
 }
