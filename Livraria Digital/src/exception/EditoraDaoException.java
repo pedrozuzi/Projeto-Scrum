b609be5e09a37f75b0756f0d9ca06abd41286331
Zuzi <pedropz0750@gmail.com>
2016-03-26 17:02:38 -0300

    exception

--- Livraria Digital/src/exception/EditoraDaoException.java
+++ Livraria Digital/src/exception/EditoraDaoException.java
@@ -3,6 +3,26 @@ package exception;
 public class EditoraDaoException extends GenericException {
 
 	private static final long serialVersionUID = -7306165201428574472L;
+
+	public EditoraDaoException() {
+		super();
+	}
+
+	public EditoraDaoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public EditoraDaoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EditoraDaoException(String message) {
+		super(message);
+	}
+
+	public EditoraDaoException(Throwable cause) {
+		super(cause);
+	}
 	
 	
 
