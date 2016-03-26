06f00337f1fb77c6034bc4474b6f9d015595b460
felipeizu <felipeizu@users.noreply.github.com>
2016-03-26 12:54:53 -0300

    Update Livro.java

--- Livraria Digital/src/model/Livro.java
+++ Livraria Digital/src/model/Livro.java
@@ -1,5 +1,32 @@
 package model;
 
+import java.util.Date;
+
 public class Livro {
 
+	private String isbn;
+	private String titulo;
+	private Date dataPublicacao;
+	
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public Date getDataPublicacao() {
+		return dataPublicacao;
+	}
+	public void setDataPublicacao(Date dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+
+	
 }
