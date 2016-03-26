48107e2210e5026bd87fe8fb563df91027ee766e
BlueTub <hury_gabriel@hotmail.com>
2016-03-26 10:39:09 -0300

    Tables, autor, editora, livro

--- Livraria Digital/sql/DataBase.sql
+++ Livraria Digital/sql/DataBase.sql
@@ -1,3 +1,48 @@
 create database livrariadigital
 go
 use livrariadigital
+
+
+
+create table autor(
+id int not null identity,
+nome varchar(100) not null,
+datanasc date not null,
+datafale date,
+localmorte varchar(100),
+--biografia
+primary key(id)
+)
+
+create table editora(
+
+id int not null identity,
+nome varchar(100) not null,
+endereco varchar(100) not null,
+tel varchar(11) not null,
+cnpj varchar(12) not null,
+
+--primary key(cnpj), --verificar
+primary key(id)
+)
+
+create table livro(
+id int not null identity,
+idautor int not null,
+ideditora int not null,
+titulo varchar(30),
+isbn varchar(12),
+paginas varchar(4),
+edicao varchar(4),
+tipocapa varchar(10),
+--info editora
+ano varchar(4),
+assunto varchar(40),
+idioma varchar(40),
+--resumo
+--sumario
+foreign key (idautor) references autor(id),
+foreign key (ideditora) references editora(id),
+primary key(id)
+)
+
