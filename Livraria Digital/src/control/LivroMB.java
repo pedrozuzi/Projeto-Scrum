03cb0ef4dd40f5c3da303be36d91140177cd86c8
Gustavo <gustavohenriqueaj@gmail.com>
2016-03-30 19:57:33 -0300

    removi os implements

--- Livraria Digital/src/control/LivroMB.java
+++ Livraria Digital/src/control/LivroMB.java
@@ -19,7 +19,7 @@ import persistence.LivroDaoImpl;
 
 @ManagedBean
 @ViewScoped
-public class LivroMB implements Serializable, GenericDao<Livro> {
+public class LivroMB implements Serializable {
 	private static final long serialVersionUID = -6883059046573310496L;
 	private Livro  livroatual;
 	private LivroDao lDao;
@@ -31,37 +31,7 @@ public class LivroMB implements Serializable, GenericDao<Livro> {
 		lDao = new LivroDaoImpl();
 	}
 	
-
-	@Override
-	public void inclui(Livro l) throws GenericException, SQLException {
-		String msg="Erro ao cadastrar!";
-		try {
-			lDao.inclui(l);
-			msg = "Cadastro concluído com sucesso!";
-			FacesContext fc = FacesContext.getCurrentInstance();
-			fc.addMessage( "", new FacesMessage( msg ) );
-		} catch (LivroDaoException lx) {
-			lx.printStackTrace();
-		}
-	}
-
-	@Override
-	public List<?> pesquisa(Livro obj) throws GenericException, SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void altera(Livro obj) throws GenericException, SQLException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void exclui(Livro obj) throws GenericException, SQLException {
-		// TODO Auto-generated method stub
-		
-	}
+	
 	
 
 }
