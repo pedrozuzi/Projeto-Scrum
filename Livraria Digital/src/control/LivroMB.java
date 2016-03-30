56560d7dba02ce9839ee7349e07c954b9c929cd3
Gustavo <gustavohenriqueaj@gmail.com>
2016-03-30 20:15:36 -0300

    arrumei o LIvro MB

--- Livraria Digital/src/control/LivroMB.java
+++ Livraria Digital/src/control/LivroMB.java
@@ -12,6 +12,7 @@ import javax.faces.context.FacesContext;
 import exception.EditoraDaoException;
 import exception.GenericException;
 import exception.LivroDaoException;
+import model.Editora;
 import model.Livro;
 import persistence.GenericDao;
 import persistence.LivroDao;
@@ -21,17 +22,58 @@ import persistence.LivroDaoImpl;
 @ViewScoped
 public class LivroMB implements Serializable {
 	private static final long serialVersionUID = -6883059046573310496L;
-	private Livro  livroatual;
+	private Livro  livroAtual;
 	private LivroDao lDao;
 	
 	
 	
 	public LivroMB(){
-		livroatual = new Livro();
+		livroAtual = new Livro();
 		lDao = new LivroDaoImpl();
 	}
 	
+	public void inclui()throws GenericException, SQLException{
+		String msg="Erro ao cadastrar!";
+		try {
+			lDao.inclui(livroAtual);
+			msg = "Cadastro concluído com sucesso!";
+			FacesContext fc = FacesContext.getCurrentInstance();
+			fc.addMessage( "", new FacesMessage( msg ) );
+		} catch (LivroDaoException lx) {
+			lx.printStackTrace();
+		}
+	}
 	
 	
+	public List<?> pesquisa(Livro obj) throws GenericException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void altera() throws GenericException, SQLException {
+		
+		String msg="Erro ao Alterar!";
+	try{	
+		lDao.altera(livroAtual);
+		msg = "Alteração realizada com sucesso!";
+		FacesContext fc= FacesContext.getCurrentInstance();
+		fc.addMessage("", new FacesMessage(msg));
+	}catch(EditoraDaoException ex){	
+		ex.printStackTrace();
+	 }
+	}
+
+	public void exclui(Livro obj) throws GenericException, SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Livro getLivroAtual() {
+		return livroAtual;
+	}
+
+	public void setLivroAtual(Livro livroatual) {
+		this.livroAtual = livroatual;
+	}
 
 }
