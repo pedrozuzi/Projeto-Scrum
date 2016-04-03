c31a5635cf698a6e0cd181d8e4cd5ab1692389d1
Zuzi <pedropz0750@gmail.com>
2016-04-03 17:27:01 -0300

    categoria

--- Livraria Digital/src/control/CategoriaMB.java
+++ Livraria Digital/src/control/CategoriaMB.java
@@ -4,12 +4,19 @@ import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
 import exception.GenericException;
 import model.Categoria;
+import model.Editora;
 import persistence.CategoriaDaoImpl;
 
 @ManagedBean
+@ViewScoped
 public class CategoriaMB extends GenericBean<Categoria> implements Serializable{
 	private static final long serialVersionUID = 1949986787432773393L;
 	
@@ -21,26 +28,79 @@ public class CategoriaMB extends GenericBean<Categoria> implements Serializable{
 
 	@Override
 	public List<Categoria> pesquisar() throws GenericException, SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String msg = "Erro";
+		try {
+			listaPesquisa = dao.pesquisa(objAtual);
+			msg = "Foram encontradas " + listaPesquisa.size() + " categorias";
+			FacesContext fc = FacesContext.getCurrentInstance();
+			fc.addMessage( "", new FacesMessage( msg ) );
+		} catch (GenericException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		return listaPesquisa;
 	}
 
 	@Override
 	public void inclui() throws GenericException, SQLException {
-		// TODO Auto-generated method stub
+		String msg="Erro ao cadastrar!";
+		try {
+			dao.inclui(objAtual);
+			msg = "Cadastro concluído com sucesso!";
+			FacesContext fc = FacesContext.getCurrentInstance();
+			fc.addMessage( "", new FacesMessage( msg ) );
+		} catch (GenericException | SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void altera(Categoria selectedObj) throws GenericException, SQLException {
-		// TODO Auto-generated method stub
+		String msg="Erro ao Alterar!";
+		try {
+			dao.altera(objAtual);
+			msg = "Alteração realizada com sucesso!";
+			FacesContext fc= FacesContext.getCurrentInstance();
+			fc.addMessage("", new FacesMessage(msg));
+		} catch (GenericException | SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void exclui(Categoria selectedObj) throws GenericException, SQLException {
-		// TODO Auto-generated method stub
+		String msg="Erro ao Excluir!";
+		try {
+			dao.exclui(objAtual);
+			msg = "Exclusão realizada com sucesso!";
+			FacesContext fc= FacesContext.getCurrentInstance();
+			fc.addMessage("", new FacesMessage(msg));
+		} catch (GenericException | SQLException e) {
+			e.printStackTrace();
+		}
 		
+	}
+	
+	@Override
+	public List<Categoria> getListaPesquisa() {
+		return listaPesquisa;
+	}
+
+	@Override
+	public void setListaPesquisa(List<Categoria> listaPesquisa) {
+		this.listaPesquisa = listaPesquisa;
+	}
+
+	@Override
+	public Categoria getObjAtual() {
+		return objAtual;
+	}
+
+	@Override
+	public void setObjAtual(Categoria objAtual) {
+		this.objAtual = objAtual;
 	}
 
 }
--- Livraria Digital/src/persistence/CategoriaDaoImpl.java
+++ Livraria Digital/src/persistence/CategoriaDaoImpl.java
@@ -1,5 +1,6 @@
 package persistence;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
