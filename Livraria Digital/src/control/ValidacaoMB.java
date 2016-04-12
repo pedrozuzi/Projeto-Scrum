797ebca907113f20f023bc1c285d469a069dda77
Gustavo <gustavohenriqueaj@gmail.com>
2016-04-12 07:11:35 -0300

    up.

---
+++ Livraria Digital/src/control/ValidacaoMB.java
@@ -0,0 +1,17 @@
+package control;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+
+@ManagedBean (name="validaMB")
+@ViewScoped
+public class ValidacaoMB {
+	
+	
+	
+	
+
+}
