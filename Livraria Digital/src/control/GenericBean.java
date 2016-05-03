5cb56f2218f50cfeb47b31ccf1a388e5d478089f
BlueTub <hury_gabriel@hotmail.com>
2016-05-03 20:17:02 -0300

    FIX LIVRO

--- Livraria Digital/src/control/GenericBean.java
+++ Livraria Digital/src/control/GenericBean.java
@@ -5,62 +5,45 @@ import java.sql.SQLException;
 import java.util.List;
 import exception.GenericException;
 import persistence.GenericDao;
+
 /**
  * 
  * @author hury
  *
  * @param <T>
  */
-abstract class GenericBean<T> implements Serializable {
+public abstract class GenericBean<T> implements Serializable {
 	private static final long serialVersionUID = -8972018181173269695L;
 
 	protected List<T> listaPesquisa;
 	protected T objAtual;
 	protected GenericDao<T> dao;
 	protected T selectedObj;
-	
+
 	abstract public List<T> pesquisar() throws GenericException, SQLException;
-	
+
 	abstract public void inclui() throws GenericException, SQLException;
-	
+
 	abstract public void altera(T selectedObj) throws GenericException, SQLException;
-	
+
 	abstract public void exclui(T selectedObj) throws GenericException, SQLException;
-/*
-	protected List<T> pesquisar() {
-		try {
-			listaPesquisa = dao.pesquisa(objAtual);
-		} catch (GenericException | SQLException e) {
-			e.printStackTrace();
-		}
 
-		return listaPesquisa;
-	}
-
-	protected void inclui() {
-		try {
-			dao.inclui(objAtual);
-		} catch (GenericException | SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	protected void altera() {
-		try {
-			dao.altera(objAtual);
-		} catch (GenericException | SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	protected void exclui() {
-		try {
-			dao.exclui(objAtual);
-		} catch (GenericException | SQLException e) {
-			e.printStackTrace();
-		}
-	}
-*/
+	/*
+	 * protected List<T> pesquisar() { try { listaPesquisa =
+	 * dao.pesquisa(objAtual); } catch (GenericException | SQLException e) {
+	 * e.printStackTrace(); }
+	 * 
+	 * return listaPesquisa; }
+	 * 
+	 * protected void inclui() { try { dao.inclui(objAtual); } catch
+	 * (GenericException | SQLException e) { e.printStackTrace(); } }
+	 * 
+	 * protected void altera() { try { dao.altera(objAtual); } catch
+	 * (GenericException | SQLException e) { e.printStackTrace(); } }
+	 * 
+	 * protected void exclui() { try { dao.exclui(objAtual); } catch
+	 * (GenericException | SQLException e) { e.printStackTrace(); } }
+	 */
 	public List<T> getListaPesquisa() {
 		return listaPesquisa;
 	}
@@ -84,7 +67,13 @@ abstract class GenericBean<T> implements Serializable {
 	public void setSelectedObj(T selectedObj) {
 		this.selectedObj = selectedObj;
 	}
-	
-	
+
+	public GenericDao<T> getDao() {
+		return dao;
+	}
+
+	public void setDao(GenericDao<T> dao) {
+		this.dao = dao;
+	}
 
 }
