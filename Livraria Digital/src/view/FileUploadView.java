f01e2b407d91c4c325b83b251c38317d573acb3a
Zuzi <pedropz0750@gmail.com>
2016-05-05 00:25:23 -0300

    upload

---
+++ Livraria Digital/src/view/FileUploadView.java
@@ -0,0 +1,28 @@
+package view;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+
+@ManagedBean
+public class FileUploadView {
+	
+	public void handleFileUpload(FileUploadEvent event) throws IOException {
+		byte[] arquivo = event.getFile().getContents();
+		String caminho = "C:\\Users\\Pedro\\git\\Projeto-Scrum\\Livraria Digital\\WebContent\\resumo\\" + event.getFile().getFileName();
+		
+		FileOutputStream fos = new FileOutputStream(caminho);
+        fos.write(arquivo);
+        fos.close();
+        
+        System.out.println("caminho da imagem salva é  = " + caminho);
+		
+        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+}
